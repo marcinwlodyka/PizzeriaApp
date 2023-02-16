859aac09e1b13bf3f2188530dd05b9709a055e3a
dead-playmod <playmod.pl@gmail.com>
2023-02-16 23:20:00 +0100

    add crud operations on menu

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -7,23 +7,54 @@ using Microsoft.AspNetCore.Mvc;
 using PizzeriaApp.Models;
 using Microsoft.Extensions.Logging;
 
-namespace PizzeriaApp.Controllers
-{
-    public class PizzaController : Controller
-    {
+namespace PizzeriaApp.Controllers {
+    public class PizzaController : Controller {
         public static Dictionary<int, Pizza> menu = new Dictionary<int, Pizza> {
             { 1, new Pizza() { Id = 1, Name = "test1", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
             { 2, new Pizza() { Id = 2, Name = "test2", Ingredients = "ass", Image="https://obiaddlataty.pl/wp-content/uploads/2020/02/domowa_pizza-scaled.jpg" } },
             { 3, new Pizza() { Id = 3, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
-            { 4, new Pizza() { Id = 3, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
-            { 5, new Pizza() { Id = 3, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
-            { 6, new Pizza() { Id = 3, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
-            { 7, new Pizza() { Id = 3, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
+            { 4, new Pizza() { Id = 4, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
+            { 5, new Pizza() { Id = 5, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
+            { 6, new Pizza() { Id = 6, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
+            { 7, new Pizza() { Id = 7, Name = "test3", Ingredients = "ass", Image="https://cdn.aniagotuje.com/pictures/articles/2022/08/31553098-v-1500x1500.jpg" } },
         };
+        
+        public static int i = 8;
 
-        public IActionResult Index()
+        public IActionResult Index() => View(menu);
+
+        public IActionResult CreateForm() => View();
+
+       [HttpPost] 
+        public IActionResult Create([FromForm] Pizza pizza) {
+            if (!ModelState.IsValid) return View("CreateForm");
+
+            menu[i] = new Pizza() { Id = i, Name = pizza.Name, Ingredients = pizza.Ingredients, Image = pizza.Image };
+            i++;
+
+            return View("Index", menu);
+        }
+
+        public IActionResult EditForm([FromRoute] int id) {
+            if (menu.ContainsKey(id)) return View(menu[id]);
+
+            return View("Index", menu);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromForm] Pizza pizza) {
+            if (!ModelState.IsValid) return View("EditForm");
+
+            menu[pizza.Id] = pizza;
+
+            return View("Index", menu);
+        }
+
+        public IActionResult Delete([FromRoute] int id)
         {
-            return View(menu);
+            menu.Remove(id);
+
+            return View("Index", menu);
         }
     }
 }
--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PizzeriaApp.Models {
     public class Pizza {
+        [HiddenInput]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Ingredients { get; set; }
